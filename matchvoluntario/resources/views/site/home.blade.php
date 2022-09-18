115ac5db77413ab7a363c8c9a6420ec38b1ffc72
Guilherme <guimoltri@gmail.com>
2022-09-18 00:39:26 -0300

    Versão Estável do Projeto

--- matchvoluntario/resources/views/site/home.blade.php
+++ matchvoluntario/resources/views/site/home.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/font-awesome.css'])
 
 
     <title>Document</title>
@@ -23,6 +23,21 @@
                     <div class="header-logo">
                         <a href="{{ route('home')}}"><span>match</span>voluntario</a>
                     </div>
+                    <button type="button" class="header-hamburguer-btn js-header-menu-toggler">
+                        <span></span>
+                    </button>
+                    <div class="header-backdrop js-header-backdrop"></div>
+                    <nav class="header-menu js-header-menu">
+                        <button type="button" class="header-close-btn js-header-menu-toggler">
+                            <i class="fas fa-times"></i>
+                        </button>
+                        <ul class="menu">
+                            <li class="menu-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="menu-item"><a href="#"></a>Ongs</li>
+                            <li class="menu-item"><a href="#"></a>Testemunhos</li>
+                            <li class="menu-item"><a href="#"></a>Contato</li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </header>
