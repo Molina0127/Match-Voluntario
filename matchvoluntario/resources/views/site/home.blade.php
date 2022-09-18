339d4aac4479eb06f9b07d5c93ff8a8f0af70b80
Guilherme <guimoltri@gmail.com>
2022-09-17 23:10:03 -0300

    Estabelecendo uma estrutura melhor de pastas e começando a home

---
+++ matchvoluntario/resources/views/site/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
+    <title>Document</title>
+</head>
+
+<body>
+    
+    <!-- main wrapper start -->
+    <div class="main-wrapper">
+
+        <!-- header start -->
+        <header class="header">
+            <div class="container">
+                <div class="header-main">
+                    <div class="header-logo">
+                        <a href="{{ route('home')}}"><span>match</span>voluntario</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+    </div>
+
+</body>
+
+</html>
