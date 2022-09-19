4aa4c8edee39540b6e44cb9f62127e1877d1f8a5
Guilherme <guimoltri@gmail.com>
2022-09-18 21:32:13 -0300

    Desenvolvimento Header Pronto, fazendo outras Sections

--- matchvoluntario/resources/views/site/home.blade.php
+++ matchvoluntario/resources/views/site/home.blade.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    @vite(
-        [
-        'resources/css/app.css', 'resources/js/app.js', 
-        ])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
     <title>Document</title>
@@ -36,16 +33,60 @@
                         </button>
                         <ul class="menu">
                             <li class="menu-item"><a href="{{ route('home') }}">Home</a></li>
-                            <li class="menu-item"><a href="#">Ongs</a></li>
-                            <li class="menu-item"><a href="#">Testemunhos</a></li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="#" class="js-toggle-sub-menu">Ongs<i class="fas fa-chevron-down"></i></a>
+                                <ul class="sub-menu js-sub-menu">
+                                    <li class="sub-menu-item"><a href="#">Ongs</a></li>
+                                    <li class="sub-menu-item"><a href="#">Detalhes Ongs</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="#" class="js-toggle-sub-menu">Pages<i class="fas fa-chevron-down"></i></a>
+                                <ul class="sub-menu js-sub-menu">
+                                    <li class="sub-menu-item"><a href="#">Login</a></li>
+                                    <li class="sub-menu-item"><a href="#">Cadastre-se</a></li>
+                                </ul>
+                            </li>
                             <li class="menu-item"><a href="#">Contato</a></li>
                         </ul>
                     </nav>
                 </div>
             </div>
         </header>
+        <!-- header end -->
+
+        <!-- banner section start -->
+        <section class="banner-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="banner-text">
+                            <h2>Encontre os melhores projetos e os melhores voluntários em um só lugar</h2>
+                            <h1>O melhor Website do Voluntariado!</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Laborum repellat ut qui veniam quibusdam tempora?
+                            </p>
+                            <a href="#" class="btn btn-theme">Faça parte dessa ação</a> 
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="banner-img">
+                            <div class="circular-img">
+                                <div class="circular-img-inner">
+                                    <div class="circular-img-circle"></div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- banner section end -->
 
     </div>
+    <!-- main wrapper end-->
+    
 
 </body>
 
