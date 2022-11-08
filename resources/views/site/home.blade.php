59f3fd86cf22e7425e994f12c44c6ab1654e0c9d
Guilherme <guimoltri@gmail.com>
2022-11-08 17:25:21 -0300

    Alterando a Home - Vite - Resources app.css

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.js'])
     
 
     <title>Home</title>
