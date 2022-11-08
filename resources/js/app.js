d4f125bdb2f418ac412b356e432469aa46567292
Guilherme <guimoltri@gmail.com>
2022-11-08 17:52:24 -0300

    Adicionando assets no app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,18 @@ import './main.js';
 
 import '../css/app.css';
 
+import '../img/Foto-banner.jpg';
+import '../img/Foto-banner-quadrangular.png';
+import '../img/Foto-banner-removebg-preview.png';
+import '../img/Foto-banner-removebg-preview-reduced.png';
 import '../img/Foto-banner-removebg-preview-reduced-200.png';
-
-import '../img/**';
+import '../img/Foto-banner-removebg-preview-reduced-300.png';
+import '../img/Foto-com-fundo-removido-quadrangular.png';
+import '../img/Foto-com-fundo-removido-quadrangular2.png';
+import '../img/Foto-de-perfil-linkedin-recortada.jpeg';
+import '../img/img-ong.jpg';
+import '../img/img-ong.png';
+import '../img/img-user.jpg';
+import '../img/user.svg';
+import '../img/user-pequeno.svg';
+import '../img/user-profile.png';
